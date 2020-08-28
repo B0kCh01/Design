bdee8bceaddc3f8e5e76ce8ea9702b680baf775d
Polarizz <polarizz39@gmail.com>
2020-08-27 20:33:01 -0700

    Attempt to toggle button

---
+++ assets/scripts/main.js
@@ -0,0 +1,19 @@
+var element = document.getElementById("selector");
+
+if (!enable) {
+    element.classList.add("selected");
+    
+} else {
+    element.classList.remove("selected");
+    
+}
+
+function click() {
+    if (enable) {
+        element.classList.add("selected");
+    }
+    else {
+        element.classList.remove("selected");
+    }
+    enable = !enable;
+}
